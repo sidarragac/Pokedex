f5d62695c324fb7247d6267a747396042f77cada
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-05-11 03:33:06 -0500

    Added view engine and project structure information

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 
 const app = express();
-const port = 80;
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(require('./routes/routes'));
 
 app.listen(port, () => {
     console.log(`Pokedex APP listening on port ${port}`)
