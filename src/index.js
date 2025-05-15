f0fef949567985c58eb6f675d5e5a098d154895b
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-05-14 22:06:50 -0500

    Added public folder path

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
 app.use(require('./routes/routes'));
+app.use(express.static(__dirname + "/public"));
 
 app.listen(port, () => {
     console.log(`Pokedex APP listening on port ${port}`)
