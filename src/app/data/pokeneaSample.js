9de30191fa7699d9d659fed707fb5414715c49f3
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-05-14 22:29:34 -0500

    Removed unnecesary ','

--- src/app/data/pokeneaSample.js
+++ src/app/data/pokeneaSample.js
@@ -3,7 +3,7 @@ const Pokenea = require('../models/Pokenea')
 const pokeneaSample = [
     new Pokenea(1, 'Alirio Villa', 178, 'Vendedor Nato', 'AlirioVilla.jpeg', 'Entre más parla, más plata pagan.'),
     new Pokenea(2, 'Braulio Del Monte', 175, 'Cuida el cafetal como si fuera oro', 'BraulioDelMonte.jpeg', 'El secreto del buen café y de la vida es el tiempo que se le dedica.'),
-    new Pokenea(3, 'Ciro Zapata', 180, 'Cura los corazones rotos con sus trovas', 'CiroZapata.jpeg', 'Trovar es llorar con ritmo y reír con intención.'),,
+    new Pokenea(3, 'Ciro Zapata', 180, 'Cura los corazones rotos con sus trovas', 'CiroZapata.jpeg', 'Trovar es llorar con ritmo y reír con intención.'),
     new Pokenea(4, 'El Mocho Palacios', 165, 'Arregla hasta lo que no está roto', 'ElMochoPalacios.jpeg', 'Uno no necesita todas las piezas para seguir andando.'),
     new Pokenea(5, 'Luzmila Ortiz', 168, 'Emprendedora rural', 'LuzmilaOrtiz.jpeg', 'El que no arriesga en la plaza, no saborea el arequipe.'),
     new Pokenea(6, 'Tarsila Montoya', 162, 'Tejedora de cuentos campesinos', 'TarsilaMontoya.jpeg', 'A veces una buena historia cura más que una medicina.'),
