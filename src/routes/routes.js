25541603d9efb329938706e8a48681aae964db46
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-05-14 23:01:18 -0500

    Created routes for pokedex

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../app/controllers/HomeController');
-
+const pokedexController = require('../app/controllers/PokedexController');
 
 router.get('/', homeController.index);
+router.get('/pokedex/json', pokedexController.pokeneaJson);
+router.get('/pokedex/imageAndPhrase', pokedexController.pokeneaImageAndPhrase);
 
 module.exports = router;
