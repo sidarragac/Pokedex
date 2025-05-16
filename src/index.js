ce493f218a46074528a12a1d52d65b96618029e3
Santiago Idárraga Ceballos <san200526@gmail.com>
2025-05-15 23:36:27 -0500

    Called app's port from the .env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
